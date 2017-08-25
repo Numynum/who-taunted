742179310795322c6bee9446e6c1a822076d7093
David Griffin <Davie3@users.noreply.github.com>
2017-08-25 20:28:54 +0000

    Fixed zhCN and zhTW locales. Adjusted package file to point directly to Ace3 folders rather than the entire Ace3 project itself.

--- Localization/zhCN.lua
+++ Localization/zhCN.lua
@@ -5,4 +5,4 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("WhoTaunted", "zhCN", true);
 if not L then return end
 
---@localization(locale="znCN", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
+--@localization(locale="zhCN", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
--- Localization/zhTW.lua
+++ Localization/zhTW.lua
@@ -5,4 +5,4 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("WhoTaunted", "zhTW", true);
 if not L then return end
 
---@localization(locale="znTW", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
+--@localization(locale="zhTW", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
