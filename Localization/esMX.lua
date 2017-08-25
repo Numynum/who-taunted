63fc3655ef943bf74fa76fe88aaa95fd11a22986
David Griffin <Davie3@users.noreply.github.com>
2017-08-25 21:06:41 +0000

    Line spacing in esMX local. Fixed Russian local to actually be Russian and not Chinese but not just Chinese, mistyped keyword for Chinese so basically nothing. HOW HAVE I NOT NOTICED THESE LOCAL BUGS FOR 8 YEARS.

--- Localization/esMX.lua
+++ Localization/esMX.lua
@@ -4,4 +4,5 @@
 
 local L = LibStub("AceLocale-3.0"):NewLocale("WhoTaunted", "esMX", true);
 if not L then return end
+
 --@localization(locale="esMX", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
--- Localization/ruRU.lua
+++ Localization/ruRU.lua
@@ -5,4 +5,4 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("WhoTaunted", "ruRU", true);
 if not L then return end
 
---@localization(locale="znCN", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
+--@localization(locale="ruRU", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
